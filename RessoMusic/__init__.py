b36189dc932132b3abc15fa9a3b39f353eb9d169
𝐒𝐔𝐃𝐄𝐄𝐏𝐁𝐎𝐓𝐒 <sudeeptelegram@gmail.com>
2026-01-03 15:07:15 +0530

    Update __init__.py

--- RessoMusic/__init__.py
+++ RessoMusic/__init__.py
@@ -1,13 +1,13 @@
 from RessoMusic.core.bot import AMBOTOP
 from RessoMusic.core.dir import dirr
-from RessoMusic.core.git import git
+#from RessoMusic.core.git import git
 from RessoMusic.core.userbot import Userbot
 from RessoMusic.misc import dbb, heroku
 
 from .logging import LOGGER
 
 dirr()
-git()
+#git()
 dbb()
 heroku()
 
@@ -24,3 +24,4 @@ Spotify = SpotifyAPI()
 Resso = RessoAPI()
 Telegram = TeleAPI()
 YouTube = YouTubeAPI()
+
